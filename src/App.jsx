ed87a50ef77956d40ba5be94ce0e22c67d7c4bfd
khalkot <72662630+khalkot@users.noreply.github.com>
2026-04-20 19:51:23 +0200

    Add products slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import About from "./components/About"
 import Header from "./components/Header"
+import Products from "./components/Products"
 
 const App = () => {
   return (
     <div className="w-full overflow-hidden">
       <Header/>
       <About/>
+      <Products/>
     </div>
   )
 }
